93860bd119a3830f1c839089fc9d63dc17621f71
Blake Scala and Stephen Emery <stephen@>
2016-10-03 15:44:27 -0700

    add getHand method to Game class

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.*;
 import org.sql2o.*;
 
 public class GameTest {
+
   @Test
   public void game_instantiates() {
     Game game = new Game("Blake");
@@ -19,5 +20,16 @@ public class GameTest {
     ArrayList<Card> cards = game.getDeck();
     assertEquals(cards.size(), 52);
     cards.clear();
+  }
+
+  @Test
+  public void getHand_returnsFiveCards() {
+    Game game = new Game("Blake");
+    ArrayList<Card> cards = game.getDeck();
+    ArrayList<Card> hand = game.getHand();
+    assertEquals(hand.size(), 5);
+    for (Card card : hand) {
+      System.out.println(card.getSuit() + card.getRank());
+    }
   }
 }
