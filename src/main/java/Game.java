44f3630c1849c5550a174fd8f438701372147309
Blake Scala and Stephen Emery <stephen@>
2016-10-03 14:37:18 -0700

    add Game class that handles generation of card deck

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -4,18 +4,17 @@ import java.util.Arrays;
 import java.util.Random;
 import org.sql2o.*;
 
-public class Deck {
-  private List<Card> allCards;
+public class Game {
+  private String playerName;
   private String[] suits = {"clubs", "spades", "hearts", "diamonds"};
   private Integer[] ranks = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+  private ArrayList newDeck = new ArrayList();
 
-  public Deck() {
-
+  public Game(String playerName) {
+    this.playerName = playerName;
   }
 
-  public ArrayList<Card> getAllCards() {
-    ArrayList newDeck = new ArrayList();
-
+  public ArrayList<Card> getDeck() {
     for (String suit : suits) {
       for(int rank : ranks) {
         Card card = new Card(suit, rank);
@@ -24,5 +23,4 @@ public class Deck {
     }
     return newDeck;
   }
-
 }
